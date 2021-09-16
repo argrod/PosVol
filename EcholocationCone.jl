6e659f9500d03bb8cb2aa68b569a08a5ebc06bfe
argrod <aran.garrod@gmail.com>
2021-09-16 18:29:21 +0900

    Got some more workings done. Now need to track and sum the total volume searched using pitch, roll, and heading

--- EcholocationCone.jl
+++ EcholocationCone.jl
@@ -17,8 +17,8 @@ dat = DataFrame(Depth = Dat.Depth[1:sum(ismissing.(Dat.Heading) .== false)], Pit
 function maxEchD(ci)
     1500*(ci/2)
 end
-function sphSecVol(R,d,θ)
-    (2/3)*pi*R^2*(d - d*cos(θ))
+function sphSecVol(d,θ)
+    (2/3)*pi*d^2*(d - d*cos(θ/2))
 end
 function ellAr(x,y)
     pi*x*y
@@ -26,8 +26,7 @@ end
 
 d = maxEchD(0.05)
 θ = 5/(180/pi)
-rad = d*tan(θ)
-V = sphSecVol(rad,d,θ)
+V = sphSecVol(d,θ)
 
 ϕ = LinRange(0, 5*(180/pi), 100)
 φ = LinRange(0, 5*(180/pi), 100)
@@ -74,3 +73,19 @@ z = sqrt.(x.^2 + y.^2);
 
 # The rstride and cstride arguments default to 10
 plot(x,y,z, st = :surface,camera = (-50,30))
+
+function rotY(θ)
+    [[cos(θ), 0, -sin(θ)] [0, 1, 0] [sin(θ), 0, cos(θ)]]
+end
+
+function rotX(θ)
+    [[1,0,0] [0,cos(θ),-sin(θ)] [0,sin(θ),cos(θ)]]
+end
+
+function rotZ(θ)
+    [[cos(θ),-sin(θ),0] [sin(θ),cos(θ),0] [0,0,1]]
+end
+
+[0,0,1].*rotY(50).*rotX(50).*rotZ(50)
+
+rotX(50).*rotY(50)
